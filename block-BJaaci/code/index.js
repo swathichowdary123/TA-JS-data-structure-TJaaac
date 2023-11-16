d0376b159cfe6125fe9cb5dbd583c212562d8677
swathichowdary123 <swathichowdary548.com>
2023-11-16 15:33:56 +0530

    combining and working with arrays and objects

--- block-BJaaci/code/index.js
+++ block-BJaaci/code/index.js
@@ -31,61 +31,66 @@ Use the variable named movies to solve the requirement below.
 */
 
 // 1. Log the value of the title key in the first movie of the array named `movies`
-
+movies[0].title
 // 2. Log the value of the title key in the last movie of the array named `movies`
-
+movies[2].title
 // 3. Log the value of the title key in the second movie (index 1) of the array named `movies`
-
+movies[1].title
 // 4. Log the value of the year key in the first movie of the array named `movies`
-
+movies[0].year
 // 5. Log all the named of actors (key named actors) of the first movie
-
+movies[0].actors
 // 6. Log the name of the first actor of the first movie
-
+movies[0].actors[0]
 // 7. Log the name of the last actor of the first movie
-
+movies[0].actors[2]
 // 8. Log the name of the second actor (index 1) of the first movie
-
+movies[0].actors[1]
 // 9. Log the name of the second actor (index 1) of the second movie
-
+movies[1].actors[1]
 // 10. Log the name of the last actor of the thrid movie
-
+movies[2].actors[2]
 // 11. Log the name of the second actor (index 1) of the third movie
-
+movies[2].actors[1]
 // 12. Log all the genres of the third movie
-
+movies[2].genres
 // 13. Log all the genres of the first movie
-
+movies[0].genres
 // 14. Log the first genres of the first movie
-
+movies[0].genres[0]
 // 15. Log the first genres of the second movie
-
+movies[1].genres[0]
 // 16. Log the last genres of the first movie (using the length property of array)
-
+movies[0].genres[2]
 // 17. Log the first genres of the second movie (using the length property of array)
-
+movies[1].genres[0]
 // 18. Log all the genres of the first movie one by one
-
+movies[0].genres
 // 19. Log all the genres of the second movie one by one
-
+movies[1].genres
 // 20. Log if the first actor of the first movie is `Rachel McAdams` or not (You have to log true or false)
-
+movies[0].actors[0]="rachel McAdams"?true:false;
 // 21. Log if the second actor (index 1) of the second movie is `Natalie Portman` or not (You have to log true or false)
-
+movies[1].actors[1]="Natalie Portman"?true:false;
 // 22. Log if the year of all three movies is greater than `2017` or not one by one. (Log true or false)
-
+movies[0],movies[1],movies[2].year>2017?true:false;
 // 23. Log the title of all three movies one by one.
-
+movies[0].title,movies[1].title,movies[2].title
 // 24. Log if the title of the first movie is `Hannah` or not
-
+movies[0].title="Hannah"?true:false;
 // 25. Log the number of actors in all three movies one by one
-
+movies[0].actors,movies[1].actors,movies[2].actors
 // 26. Log the number of genres in all three movies one by one
-
+movies[0].genres,movies[1].genres,movies[2].genres
 // 27. Log the name of all the movies with more than 1 genre
-
+movies[0].genres[0], genres[1]
+movies[1].genres[0], genres[1]
+movies[2].genres[0], genres[1]
 // 28. Log the name of all the movies with more than 1 actors
-
+movies[0].actors[0], actors[1]
+movies[1].actors[0], actors[1]
+movies[2].actors[0], actors[1]
 // 29. Log the name of all the movies with exactly 3 actors
-
+movies[0].actors,movies[1].actors,movies[2].actors
 // 30. Log the name of all the movies with year `2018`
+movies[0].year,movies[1].year
