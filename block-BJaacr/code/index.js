9ed0d9722229b0922bdf5d084b09f84eee91aa79
swathichowdary123 <swathichowdary548.com>
2023-11-20 12:27:37 +0530

    complete assignment combining array and objets

--- block-BJaacr/code/index.js
+++ block-BJaacr/code/index.js
@@ -19,29 +19,32 @@ Hint: (Structure of got variable)
 */
 
 // 1. Log the value of key "houses"
-
+console.log(got.houses);
 // 2. Log the length of the "houses" key
-
+console.log(got.houses.length);
 // 3. Log the name of all houses in got one by one
-
+console.log(got.houses[0].name);
+console.log(got.houses[1].name);
 // 4. Log the name of all the people in house Starks (index 0) one by one
-
+console.log(got.houses[0].people.name);
 // 5. Log the name of all the people in house Lannisters (index 1) one by one
-
+console.log(got.houses[1].people.name);
 // 6. Log the description of all the people in house Lannisters (index 1) one by one
-
+console.log(got.houses[1].people.description);
 // 7. Log the name and description of all the people in house Lannisters (index 1) one by one like `I am [name] and my bio is [description]`
 
 // 8. Log the name and description of all the people in house Starks (index 0) one by one like `I am [name] and my bio is [description]`
 
 // 9. Log the name and description of the people in house Starks whose name is `Robb Stark`
-
+console.log(got.houses[0].people[2]);
 // 10. Log the name and description of the people in house Lannisters whose name is `Tywin Lannister`
-
+console.log(got.houses[1].people[0]);
 // 11. Push the name of the houses in an array named `houseNames` and Log the array
-
+let houseNames=[];
+houseNames.push(houses.name);
+console.log(houseNames);
 // 12. Log the size of people in Starks house
-
+console.log(got.houses[0].length);
 // 13. Log the size of people in Lannisters house
 
 // 14. Add the name and size of people in an object like {Starks: 4, Lannisters: 6} and log the object
@@ -49,11 +52,13 @@ Hint: (Structure of got variable)
 // 15. Log the name of all the people of all the houses in got one by one
 
 // 16. Push all names into a new array named `allPeople` and log the value array.
-
+let allPeople=[];
+console.log(houseNames.push(houses.name));
 // 17. Log the value and length of `allPeople` array created above
-
+console.log(allPeople.length);
 // 18. Push all descriptions into an array named `allDescription`
-
+let allDescription=[];
+console.log(houseNames.push(houses.description));
 // 19. Log the value and length `allDescription`
 
 // 20. Only the the description whose length is more than 30. (use .length property on string to get the length)
